0c8a8c3c05911091bb409475bb44d6a9f35c0f12
nasusLLL <lllyutong@gmail.com>
2018-10-11 19:28:51 -0700

    Initial commit

---
+++ Assets/script/Movecontrol.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movecontrol : MonoBehaviour {
+    public float speed;
+    public float rotationspeed;
+    public KeyCode shootKey;
+    public string horizontalAxie;
+    public string verticalAxie;
+    public bulletControll bulletPrefab;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizantalInput = Input.GetAxis(horizontalAxie);
+        float VerticalInput = Input.GetAxis(verticalAxie);
+
+        Vector2 vel = new Vector2(0, VerticalInput * speed);
+
+        //will rotate the provided vector by the rotation of your object
+        //we use it that even when the object is rotated,pressing up will make this move forward
+        Vector2 velbasedOnRotation = transform.TransformVector(vel);
+
+
+        rb.velocity = velbasedOnRotation;
+
+        transform.Rotate(0, 0, -horizantalInput * rotationspeed);
+
+        if(Input.GetKeyDown(shootKey))
+        {
+            Shoot();
+        }
+	}
+    void Shoot()
+    {
+       bulletControll bulletClone = (bulletControll)Instantiate(bulletPrefab, transform.position,transform.rotation);
+       bulletClone.owner = this;
+    }
+}
---
+++ Assets/script/bulletControll.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class bulletControll : MonoBehaviour {
+    public float speed;
+    private float lifetime;
+    public GameObject prefab;
+    public Movecontrol owner;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+        lifetime = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Movecontrol playerHit = col.GetComponent<Movecontrol>();
+        if(playerHit!=null)
+        {
+            
+            if (playerHit == owner)
+            {
+                return;
+            }
+            print(owner.name + "has won!");
+            SceneManager.LoadScene(0);
+        }
+       /* if(col.gameObject.tag!="Player")
+        {
+            Destroy(this.gameObject);
+            Crash();
+        }*/
+        Destroy(this.gameObject);
+        Crash();
+    }
+    void Crash()
+    {
+       Object gameobject =  Instantiate(prefab,this.transform.position,this.transform.rotation);
+       Destroy(gameobject, lifetime);
+    }
+}
